1a79dc9c64e989ec39866be2f5d68a1a2527dc4e
kurihara-yas <kurihara-ya+gitlab@ibjapan.jp>
2019-10-29 20:02:24 +0900

    Add tag

---
+++ src/components/Tag/CheckWIthTag/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+
+export interface CheckWithTagProps {
+  label: string;
+  onPress: void;
+}
+
+/**
+ * CheckWithTag
+ * check with tag
+ * todo: checked時のチェック画像
+ * @param param
+ *  label: [String]
+ *  onPress: [Function]
+ */
+const CheckWithTag: React.FC<CheckWithTagProps> = ({
+  label,
+  onPress
+}) => (
+  <TouchableOpacity style={styles.touchable} onPress={onPress}>
+    <View style={styles.tag}>
+      <Text style={styles.text}>{label}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  touchable: {
+    marginLeft: 4,
+    marginRight: 4,
+    marginBottom: 8,
+  },
+  tag: {
+    flexDirection: 'row',
+    borderRadius: 24,
+    borderColor: '#ccc',
+    borderWidth: 2,
+    backgroundColor: '#fff',
+    height: 46,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  text: {
+    textAlign: 'center',
+    color: '#ccc',
+    fontSize: 16
+  },
+});
+
+export default CheckWithTag
--- src/components/Tag/DefaultTag/index.tsx
+++ src/components/Tag/DefaultTag/index.tsx
@@ -1,29 +1,49 @@
 import React from "react";
-import { Checkbox } from 'galio-framework';
+import { View, Text, StyleSheet } from "react-native";
 
 
-export interface DefaultCheckBoxProps {
+export interface DefaultCheckTagProps {
   label: string;
   onChange: void;
 }
 
 /**
- * DefaultCheckBox
- * Checkbox default
+ * DefaultCheckTag
+ * tag default
  * https://galio.io/docs/#/components/checkbox
  * @param param
  *  label: [String]
  *  onChange: [Function]
  */
-const DefaultCheckBox: React.FC<DefaultCheckBoxProps> = ({
-  label,
-  onChange
+const DefaultCheckTag: React.FC<DefaultCheckTagProps> = ({
+  label
 }) => (
-  <Checkbox
-    label={label}
-    color="warning"
-    onChange={onChange}
-  />
+  <View style={styles.tag}>
+    <Text style={styles.text}>{label}</Text>
+  </View>
 );
 
-export default DefaultCheckBox
+const styles = StyleSheet.create({
+  tag: {
+    marginLeft: 4,
+    marginRight: 4,
+    marginBottom: 8,
+    flexDirection: 'row',
+    borderRadius: 4,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  text: {
+    textAlign: 'center',
+    color: '#ccc',
+    fontSize: 12
+  },
+});
+
+export default DefaultCheckTag
